490bdd044ebbd10e29ae2d89d0fadaf05a4a5760
Connor Lord <cpf006@gmail.com>
2020-09-25 04:15:25 -0400

    Build basic video tag manager

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VideoTagManager :video-id="videoId" :tags="tags"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VideoTagManager from './components/VideoTagManager.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    VideoTagManager
+  },
+  // Data that could come from API
+  data() {
+    return {
+      videoId: 'iHibnmosKkM',
+      tags: [{
+        tag: 'Human',
+        start: 0,
+        end: 6.8,
+      }, {
+        tag: 'Ball',
+        start: 2,
+        end: 2.5,
+      }, {
+        tag: 'Axe',
+        start: 6.5,
+        end: 6.8,
+      }, {
+        tag: 'The End',
+        start: 6.81,
+        end: 10,
+      }]
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
